9fbd11767a6511cc44db87d3c84a8daa82885de7
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-17 16:59:15 -0300

    chore: firebase configuration

--- src/main.js
+++ src/main.js
@@ -1,6 +1,19 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBNI-LIKJgqqNETBXDGNXw5dcRoYBiY1Xw",
+  authDomain: "the-shire-eb558.firebaseapp.com",
+  projectId: "the-shire-eb558",
+  storageBucket: "the-shire-eb558.appspot.com",
+  messagingSenderId: "891665356224",
+  appId: "1:891665356224:web:edf45bac6411769b0a6425"
+};
 
 new Vue({
   render: h => h(App),
+  created() {
+    initializeApp(firebaseConfig)
+  }
 }).$mount('#app')
