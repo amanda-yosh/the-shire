50c736f5a637667fd98cccd048465f98563e035f
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-18 15:17:17 -0300

    chore: remove qs and add village house

--- src/api/glue-api.js
+++ src/api/glue-api.js
@@ -1,14 +1,34 @@
 import axios from 'axios'
-import qs from 'qs'
+// import qs from 'qs'
 
 const ROOT_URL = 'https://glue-api.zapimoveis.com.br/v2/listings?categoryPage=RESULT'
-// https://glue-api.zapimoveis.com.br/v2/listings?business=SALE&listingType=USED&portal=ZAP&categoryPage=RESULT&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE&text=Casa+de+Vila&addressCountry=&addressState=&addressCity=&addressZone=&addressNeighborhood=&addressStreet=&addressAccounts=&addressType=&addressLocationId=&addressPointLat=&addressPointLon=&addressUrl=&size=24&from=0
+const villageHouse = '&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE'
+// &business=SALE
+// &listingType=USED
+// &portal=ZAP
+// &text=Casa+de+Vila
+// &addressCountry=
+// &addressState=
+// &addressCity=
+// &addressZone=
+// &addressNeighborhood=
+// &addressStreet=
+// &addressAccounts=
+// &addressType=
+// &addressLocationId=
+// &addressPointLat=
+// &addressPointLon=
+// &addressUrl=
+// &size=24
+// &from=0
 
 export function getAdvertisers(searchTerm) {
-  const searchTermStringified = qs.stringify(searchTerm)
+  // const searchTermStringified = qs.stringify(searchTerm)
+  const searchTermStringified = new URLSearchParams(searchTerm)
+
 
   axios
-    .get(`${ROOT_URL}&unitSubTypes=${searchTermStringified}`, {
+    .get(`${ROOT_URL}${villageHouse}${searchTermStringified}`, {
       headers: {
         'x-domain': 'www.zapimoveis.com.br',
       },
