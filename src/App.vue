d8ab9fcdebb514d4bdaa4a99ebbd29b4dc6bd4b7
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-18 07:07:02 -0300

    chore: receive event from searchbar and get data

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,44 @@
 <template>
   <div id="app">
+    <SearchBar @termChange="onTermChange"></SearchBar>
     Hello, Amanda here!
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+import SearchBar from './components/SearchBar.vue'
+
 export default {
   name: 'App',
+
+  components: {
+    SearchBar,
+  },
+
+  data() {
+    return {
+      advertsList: Array
+    }
+  },
+
+  methods: {
+    // https://glue-api.zapimoveis.com.br/v2/listings?business=SALE&listingType=USED&portal=ZAP&categoryPage=RESULT&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE&text=Casa+de+Vila&addressCountry=&addressState=&addressCity=&addressZone=&addressNeighborhood=&addressStreet=&addressAccounts=&addressType=&addressLocationId=&addressPointLat=&addressPointLon=&addressUrl=&size=24&from=0
+    onTermChange: function (searchTerm) {
+      axios
+        .get('https://glue-api.zapimoveis.com.br/v2/listings?', {
+          headers: {
+            'x-domain': 'www.zapimoveis.com.br',
+            'access-control-allow-origin': 'https://www.zapimoveis.com.br',
+            q: searchTerm
+          }
+        }) // return a promise
+        .then(response => {
+          console.log(response)
+          // this.advertsList = response.data
+        })
+    },
+  }
 }
 </script>
 
