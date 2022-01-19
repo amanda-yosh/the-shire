d319e9c634f8ca1281eb6d6c2d5c4b1c45b73904
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-19 12:51:06 -0300

    refactor: change function name and use helpers

--- src/api/glue-api.js
+++ src/api/glue-api.js
@@ -1,15 +1,10 @@
-import axios from 'axios'
+import { get } from '../helpers/request-helper'
 
 const ROOT_URL = 'https://glue-api.zapimoveis.com.br/v2/listings?categoryPage=RESULT'
 const villageHouse = '&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE'
 
-export async function getAdvertisers(searchTerm) {
+export async function getListings(searchTerm) {
   const searchTermStringified = new URLSearchParams(searchTerm)
-  const response = await axios
-    .get(`${ROOT_URL}${villageHouse}${searchTermStringified}`, {
-      headers: {
-        'x-domain': 'www.zapimoveis.com.br',
-      },
-    }) // return a promise
-    return response.data.search.result.listings
+  const response = await get(`${ROOT_URL}${villageHouse}${searchTermStringified}`)
+  return response.data.search.result.listings
 }
