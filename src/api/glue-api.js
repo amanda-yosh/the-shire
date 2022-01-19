525acbc33991a4e375c1c40bd5d40ea9a9d77a05
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-19 13:43:40 -0300

    chore: add constants file

--- src/api/glue-api.js
+++ src/api/glue-api.js
@@ -1,10 +1,10 @@
 import { get } from '../helpers/request-helper'
+import { BASE_URL } from '../settings/constants'
 
-const ROOT_URL = 'https://glue-api.zapimoveis.com.br/v2/listings?categoryPage=RESULT'
 const villageHouse = '&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE'
 
 export async function getListings(searchTerm) {
   const searchTermStringified = new URLSearchParams(searchTerm)
-  const response = await get(`${ROOT_URL}${villageHouse}${searchTermStringified}`)
+  const response = await get(`${BASE_URL}${villageHouse}${searchTermStringified}`)
   return response.data.search.result.listings
 }
