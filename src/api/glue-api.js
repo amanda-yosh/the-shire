d87575490557c254a1f458d5a8a7c3f9f7c28c8f
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-19 11:56:12 -0300

    refactor: remove comments and not used qs library

--- src/api/glue-api.js
+++ src/api/glue-api.js
@@ -1,29 +1,9 @@
 import axios from 'axios'
-// import qs from 'qs'
 
 const ROOT_URL = 'https://glue-api.zapimoveis.com.br/v2/listings?categoryPage=RESULT'
 const villageHouse = '&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE'
-// &business=SALE
-// &listingType=USED
-// &portal=ZAP
-// &text=Casa+de+Vila
-// &addressCountry=
-// &addressState=
-// &addressCity=
-// &addressZone=
-// &addressNeighborhood=
-// &addressStreet=
-// &addressAccounts=
-// &addressType=
-// &addressLocationId=
-// &addressPointLat=
-// &addressPointLon=
-// &addressUrl=
-// &size=24
-// &from=0
 
 export async function getAdvertisers(searchTerm) {
-  // const searchTermStringified = qs.stringify(searchTerm)
   const searchTermStringified = new URLSearchParams(searchTerm)
   const response = await axios
     .get(`${ROOT_URL}${villageHouse}${searchTermStringified}`, {
