a8a80f98fe9c1cf0a9ff88b5b1fba09ad1861925
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-26 14:18:55 -0300

    fix: use single quote

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  "root": true,
-  "env": {
-    "node": true,
-    "browser": true
+  'root': true,
+  'env': {
+    'node': true,
+    'browser': true
   },
-  "extends": [
-    "plugin:vue/essential",
-    "eslint:recommended"
+  'extends': [
+    'plugin:vue/essential',
+    'eslint:recommended'
   ],
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "parser": "babel-eslint"
+  'parserOptions': {
+    'ecmaVersion': 6,
+    'sourceType': 'module',
+    'parser': 'babel-eslint'
   },
-  "rules": {
-    "indent": ["error", 2],
-    "quotes": ["error", "single"],
-    "semi": ["error", "never"],
+  'rules': {
+    'indent': ['error', 2],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'never'],
     'vue/multi-word-component-names': 0
   }
 }
