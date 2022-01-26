1cf4deec9b09d40b1ac4a71f2d233edad15cc32c
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-26 09:49:48 -0300

    chore: prevent production start message

--- src/main.js
+++ src/main.js
@@ -5,29 +5,7 @@ import Home from './pages/Home/Home.vue'
 import Detail from './pages/Detail/Detail.vue'
 import store from './store'
 
-Vue.use(VueRouter) // tells Vue that VouRouter exists
-
-const routes = [
-  {
-    path: '/home',
-    component: Home
-  },
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/listing/:id',
-    name: 'listing',
-    component: Detail
-  }
-]
-
-// create the router instance and pass the `routes` option
-export const router = new VueRouter({
-  mode: 'history',
-  routes
-})
+Vue.config.productionTip = false // Preventing production start message
 
 new Vue({
   router,
