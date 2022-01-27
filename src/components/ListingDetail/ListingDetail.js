4812c1f0014881a6fb641888d6c31a08fdb2c625
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-27 15:12:46 -0300

    chore: add carousel

--- src/components/ListingDetail/ListingDetail.js
+++ src/components/ListingDetail/ListingDetail.js
@@ -1,13 +1,31 @@
 import { getListing } from '../../api/glue-api'
 import { mapGetters } from 'vuex'
+import VueSlickCarousel from 'vue-slick-carousel'
+import 'vue-slick-carousel/dist/vue-slick-carousel.css'
+// optional style for arrows & dots
+import 'vue-slick-carousel/dist/vue-slick-carousel-theme.css'
 
 export default ({
   name: 'ListingDetail',
 
+  components: {
+    VueSlickCarousel,
+  },
+
   data() {
     return {
       isLoading: true,
-      listing: {}
+      listing: {},
+      settings: {
+        arrows: true,
+        // dots:true,
+        // dotsClass: 'slick-dots custom-dot-class',
+        edgeFriction: 0.35,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
     }
   },
 
