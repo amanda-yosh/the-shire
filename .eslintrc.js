01a0abc1eced61fd84ad7d464da45a39874abcb8
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-01-28 15:09:56 -0300

    chore: install @vue/test-utils

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,38 @@
 module.exports = {
   'root': true,
+
   'env': {
     'node': true,
     'browser': true
   },
+
   'extends': [
     'plugin:vue/essential',
     'eslint:recommended'
   ],
+
   'parserOptions': {
     'ecmaVersion': 6,
     'sourceType': 'module',
     'parser': 'babel-eslint'
   },
+
   'rules': {
     'indent': ['error', 2],
     'quotes': ['error', 'single'],
     'semi': ['error', 'never'],
     'vue/multi-word-component-names': 0
-  }
+  },
+
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)'
+      ],
+      env: {
+        jest: true
+      }
+    }
+  ]
 }
