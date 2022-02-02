e51f2b7d5eee7848f3fd74c61f1928e51cdc832c
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-02-02 09:01:37 -0300

    chore: add login and signup routes

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,5 +1,7 @@
 import Home from '../pages/Home/Home.vue'
 import Detail from '../pages/Detail/Detail.vue'
+import Login from '../pages/Login/Login.vue'
+import SignUp from '../pages/SingUp/SignUp.vue'
 
 export const routes = [
   {
@@ -14,5 +16,15 @@ export const routes = [
     path: '/listing/:id',
     name: 'listing',
     component: Detail
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignUp
   }
 ]
