a10887ddb7a15258d1a69764b50537a1a6adbd31
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-02-03 16:06:20 -0300

    chore: add Header to Detail page

--- src/pages/Detail/Detail.vue
+++ src/pages/Detail/Detail.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
+    <Header />
     <ListingDetail />
   </div>
 </template>
 
 <script>
+import Header from '../../components/Header/Header.vue'
 import ListingDetail from '../../components/ListingDetail/ListingDetail.vue'
 
 export default ({
   name: 'Detail',
 
   components: {
+    Header,
     ListingDetail
   }
 })
