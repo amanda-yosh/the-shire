620f872804104b7bc47ffe0493f173c67b72d43e
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-02-03 18:19:51 -0300

    chore: create goToPage function in utils

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,7 +1,14 @@
+import { router } from '../router'
+
 function normalizeImageUrl(url, replaceValue) {
   return url.replace('/{action}/{width}x{height}/', `${replaceValue}`)
 }
 
+function goToPage(page) {
+  return router.push({name: page})
+}
+
 export default {
-  normalizeImageUrl
+  normalizeImageUrl,
+  goToPage
 }
