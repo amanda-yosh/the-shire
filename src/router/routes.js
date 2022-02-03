2ced39304a03c828a1f54e5512e2abc403eaa4e2
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-02-03 18:22:40 -0300

    chore: create Profile page

--- src/router/routes.js
+++ src/router/routes.js
@@ -2,10 +2,12 @@ import Home from '../pages/Home/Home.vue'
 import Detail from '../pages/Detail/Detail.vue'
 import Login from '../pages/Login/Login.vue'
 import SignUp from '../pages/SingUp/SignUp.vue'
+import Profile from '../pages/Profile/Profile.vue'
 
 export const routes = [
   {
     path: '/home',
+    name: 'home',
     component: Home
   },
   {
@@ -26,5 +28,13 @@ export const routes = [
     path: '/signup',
     name: 'signup',
     component: SignUp
+  },
+  {
+    path: '/profile',
+    name: 'profile',
+    component: Profile,
+    meta: {
+      requiresAuth: true
+    }
   }
 ]
