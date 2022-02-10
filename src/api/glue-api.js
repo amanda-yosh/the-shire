d27245c3e2e6c5b26827a8cdb71968f18fe34a9c
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-02-10 20:00:29 -0300

    wip: upgrade search

--- src/api/glue-api.js
+++ src/api/glue-api.js
@@ -3,13 +3,17 @@ import { BASE_URL } from '../settings/constants'
 
 const resultPage = '/listings?categoryPage=RESULT'
 const listingPage = '/page'
-const villageHouse = '&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE'
+const villageHouse = '&unitSubTypes=VILLAGE_HOUSE&unitTypes=HOME&usageTypes=RESIDENTIAL&unitTypesV3=VILLAGE_HOUSE&text=Casa+de+Vila'
+
+// urlBuild function
 
 export async function getListings(searchTerm) {
-  const searchTermStringified = new URLSearchParams(searchTerm)
+  // const searchTermStringified = new URLSearchParams(searchTerm)
+  // &addressState=Rio+de+Janeiro
+  // const search = `&business=${business}&addressCity=${searchCity}`
 
   try {
-    const response = await get(`${BASE_URL}${resultPage}${villageHouse}${searchTermStringified}`)
+    const response = await get(`${BASE_URL}${resultPage}${villageHouse}${searchTerm}`)
     return response.data.search.result.listings
   } catch (error) {
     console.log(error)
--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,9 +1,34 @@
+import { mapActions } from 'vuex'
+
 export default {
   name: 'SearchBar',
+
+  data() {
+    return {
+      businessSearch: 'SALE',
+      citySearch: '',
+      isSaleActive: true,
+    }
+  },
   
   methods: {
-    onInput: function (event) {
-      this.$emit('termChange', event.target.value)
+    ...mapActions(['setListings']),
+
+    changeBusinnes: function (event) {
+      this.businessSearch = event.target.value
+      this.isSaleActive = !this.isSaleActive
     },
+
+    onInputCity: function (event) {
+      this.citySearch = event.target.value
+    },
+
+    onSubmit: function () {
+      let city = this.citySearch
+      city = city.replaceAll(' ', '+')
+      const searchTerm = `&business=${this.businessSearch}&addressCity=${city}`
+
+      this.setListings(searchTerm)
+    }
   },
 }
