c0a644c0ef11ee65473c3b7f2c11d02bdc83120b
Amanda Maria Costa de Almeida <amandamca.aa@gmail.com>
2022-02-11 15:47:00 -0300

    fix: rename variables

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -3,7 +3,7 @@ import { getAuth } from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: process.env.VUE_APP_API_KEY,
-  authDomain: process.env.VUE_APP_AUT_DOMAIN,
+  authDomain: process.env.VUE_APP_AUTH_DOMAIN,
   projectId: process.env.VUE_APP_PROJECT_ID,
   storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_MESSAGING_SENDER_ID,
